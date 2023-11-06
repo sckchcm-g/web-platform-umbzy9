c2f47d481660d336b21ee1b2dbd13cc77f9e906e
sckchcm-g <141105618+sckchcm-g@users.noreply.github.com>
2023-11-06 18:01:26 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,19 @@
+// Progression 1: Create special addition
+function specialAddition(numberOne, numberTwo){
+  return numberOne<0 | numberTwo<0 ?"Negative numbers are not allowed":numberOne-numberTwo;
+}
+
+// function specialAddition(){}
+
+// Progression 2: Create simple division
+function simpleDivision(numberOne, numberTwo){
+  return numberOne<0 | numberTwo<0 ? "Negative numbers are not allowed" : 
+      numberTwo==0 ? "Not a number": 
+          numberTwo>numberOne ? "Cannot divide a smaller number from a larger number": numberOne/numberTwo;
+}
+// Progression 3: Create the special calculator
+
+function specialCalculator(numberOne, numberTwo, operation){
+  return operation(numberOne,numberTwo);
+}
+
